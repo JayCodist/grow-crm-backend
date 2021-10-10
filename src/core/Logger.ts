d91ae2ac86f797d0a90c37b235cf8f4379cc81df
Johnson Ubaezuonu <jaycodist@gmail.com>
2021-10-10 15:38:02 +0100

    fixed typo

--- src/core/Logger.ts
+++ src/core/Logger.ts
@@ -1,50 +1,50 @@
-import { createLogger, transports, format } from "winston";
-import fs from "fs";
-import path from "path";
-import DailyRotateFile from "winston-daily-rotate-file";
-import { environment, logDirectory } from "../config";
+// import { createLogger, transports, format } from "winston";
+// import fs from "fs";
+// import path from "path";
+// import DailyRotateFile from "winston-daily-rotate-file";
+// import { environment, logDirectory } from "../config";
 
-let dir = logDirectory;
-if (!dir) dir = path.resolve("logs");
+// let dir = logDirectory;
+// if (!dir) dir = path.resolve("logs");
 
-// create directory if it is not present
-if (!fs.existsSync(dir)) {
-  // Create the directory if it does not exist
-  fs.mkdirSync(dir);
-}
+// // create directory if it is not present
+// if (!fs.existsSync(dir)) {
+//   // Create the directory if it does not exist
+//   fs.mkdirSync(dir);
+// }
 
-const logLevel = environment === "development" ? "debug" : "warn";
+// const logLevel = environment === "development" ? "debug" : "warn";
 
-const options = {
-  file: {
-    level: logLevel,
-    filename: `${dir}/%DATE%.log`,
-    datePattern: "YYYY-MM-DD",
-    zippedArchive: true,
-    timestamp: true,
-    handleExceptions: true,
-    humanReadableUnhandledException: true,
-    prettyPrint: true,
-    json: true,
-    maxSize: "20m",
-    colorize: true,
-    maxFiles: "14d"
-  }
-};
+// const options = {
+//   file: {
+//     level: logLevel,
+//     filename: `${dir}/%DATE%.log`,
+//     datePattern: "YYYY-MM-DD",
+//     zippedArchive: true,
+//     timestamp: true,
+//     handleExceptions: true,
+//     humanReadableUnhandledException: true,
+//     prettyPrint: true,
+//     json: true,
+//     maxSize: "20m",
+//     colorize: true,
+//     maxFiles: "14d"
+//   }
+// };
 
-const devLog = createLogger({
-  transports: [
-    new transports.Console({
-      level: logLevel,
-      format: format.combine(
-        format.errors({ stack: true }),
-        format.prettyPrint()
-      )
-    })
-  ],
-  exceptionHandlers: [new DailyRotateFile(options.file)],
-  exitOnError: false // do not exit on handled exceptions
-});
+// const devLog = createLogger({
+//   transports: [
+//     new transports.Console({
+//       level: logLevel,
+//       format: format.combine(
+//         format.errors({ stack: true }),
+//         format.prettyPrint()
+//       )
+//     })
+//   ],
+//   exceptionHandlers: [new DailyRotateFile(options.file)],
+//   exitOnError: false // do not exit on handled exceptions
+// });
 
 const prodLog = {
   info: console.log,
@@ -53,4 +53,6 @@ const prodLog = {
   warn: console.warn
 };
 
-export default environment === "development" ? devLog : prodLog;
+export default prodLog;
+
+// export default environment === "development" ? devLog : prodLog;
