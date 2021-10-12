e87f0bc599252881fb56554b57c9f4687cb621c9
Johnson Ubaezuonu <jaycodist@gmail.com>
2021-10-12 21:12:47 +0100

    implemented gcloud build triggers

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
+require("dotenv").config();
+
 type Environment = "production" | "development";
 
 // Mapper for environment variables
-export const environment: Environment = process.env.NODE_ENV as Environment;
+export const environment: Environment =
+  (process.env.NODE_ENV as Environment) || "development";
 export const port = process.env.PORT || "8080";
 
 export const db = {
@@ -12,19 +15,13 @@ export const db = {
   password: process.env.DB_USER_PWD || ""
 };
 
-export const corsUrl = process.env.CORS_URL;
+export const corsUrl = "*";
 
 export const tokenInfo = {
-  accessTokenValidityDays: parseInt(
-    process.env.ACCESS_TOKEN_VALIDITY_SEC || "0",
-    10
-  ),
-  refreshTokenValidityDays: parseInt(
-    process.env.REFRESH_TOKEN_VALIDITY_SEC || "0",
-    10
-  ),
-  issuer: process.env.TOKEN_ISSUER || "",
-  audience: process.env.TOKEN_AUDIENCE || ""
+  accessTokenValidityDays: 30,
+  refreshTokenValidityDays: 120,
+  issuer: "https://grow-crm.web.app",
+  audience: "https://grow-crm.web.app"
 };
 
-export const logDirectory = process.env.LOG_DIR;
+export const logDirectory = "./logs";
