a3d72aa11b8f5474389b73463bfffa612f0e4107
Johnson Ubaezuonu <jaycodist@gmail.com>
2021-10-14 18:27:46 +0100

    updated cors config

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(
   express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
 );
 app.use(express.json({ limit: "10mb" }));
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 
 // Routes
 app.use("/v1", routesV1);
