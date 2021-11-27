d13db09bcaa6b56c44e213274aebbd1fcc810d58
Johnson Ubaezuonu <jaycodist@gmail.com>
2021-11-27 10:29:56 +0100

    added support for ios version validation

--- src/database/repository/MobileVersionCheckRepo.ts
+++ src/database/repository/MobileVersionCheckRepo.ts
@@ -1,6 +1,8 @@
 import {
   BlacklistVersionAndroidModel,
+  BlacklistVersionIOSModel,
   LastBreakingChangeAndroidModel,
+  LastBreakingChangeIOSModel,
   Version
 } from "../model/MobileVersions";
 
@@ -29,13 +31,30 @@ export default class MobileVersionCheckRepo {
     ] = await Promise.all([responseLBC, responseBLV]);
     const lastBreakingVersion = breakingChanges[0]?.versionCode;
     return (
-      !matchingBlacklistedVersion &&
-      (lastBreakingVersion || Infinity) <= versionCode
+      !matchingBlacklistedVersion && (lastBreakingVersion || 0) <= versionCode
     );
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public static async checkIOSVersion(versionCode: number): Promise<boolean> {
-    return false;
+    const responseLBC = LastBreakingChangeIOSModel.find()
+      .sort(latestVersionSort)
+      .limit(1)
+      .lean<Version[]>()
+      .select(versionProjection)
+      .exec();
+    const responseBLV = BlacklistVersionIOSModel.findOne({
+      versionCode
+    })
+      .lean<Version>()
+      .select(versionProjection)
+      .exec();
+    const [breakingChanges, matchingBlacklistedVersion]: [
+      Version[],
+      Version | null
+    ] = await Promise.all([responseLBC, responseBLV]);
+    const lastBreakingVersion = breakingChanges[0]?.versionCode;
+    return (
+      !matchingBlacklistedVersion && (lastBreakingVersion || 0) <= versionCode
+    );
   }
 }
