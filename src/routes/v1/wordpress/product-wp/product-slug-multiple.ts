f016406c6c13a5fd09e9f861789675adf71bc38b
Johnson Ubaezuonu <jaycodist@gmail.com>
2022-11-29 02:07:02 +0100

    created endpoint for fetching multiple products by slugs

---
+++ src/routes/v1/wordpress/product-wp/product-slug-multiple.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { ApiError } from "../../../../core/ApiError";
+import {
+  BadRequestResponse,
+  SuccessResponse
+} from "../../../../core/ApiResponse";
+import ProductWPRepo from "../../../../database/repository/ProductWPRepo";
+import validator from "../../../../helpers/validator";
+import validation from "./validation";
+
+const productWPSlugMultiple = express.Router();
+
+productWPSlugMultiple.use(
+  "/",
+  validator(validation.slugMultiple, "query"),
+  async (req, res) => {
+    try {
+      const slugs = (req.query.slugs as string)
+        .split(",")
+        .map(slug => slug.trim().toLowerCase());
+      const response = await ProductWPRepo.findBySlugs(slugs);
+
+      if (!response) {
+        new BadRequestResponse("Products not found").send(res);
+      }
+      new SuccessResponse("success", response).send(res);
+    } catch (error) {
+      ApiError.handle(error as Error, res);
+    }
+  }
+);
+
+export default productWPSlugMultiple;
