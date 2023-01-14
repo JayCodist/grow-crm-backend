788f0d791d362d8774df90595ba9143fa287d377
Johnson Ubaezuonu <jaycodist@gmail.com>
2023-01-14 12:54:40 +0100

    fixed bugs with monnify integration

--- src/routes/v1/regal-flowers/payments/index.ts
+++ src/routes/v1/regal-flowers/payments/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import verifyMonnify from "./verify-monnify";
 import verifyPaystack from "./verify-paystack";
 
 const paymentRoutes = express.Router();
 
 paymentRoutes.use("/paystack/verify", verifyPaystack);
 
-paymentRoutes.use("/monnify/verify", verifyPaystack);
+paymentRoutes.use("/monnify/verify", verifyMonnify);
 
 export default paymentRoutes;
