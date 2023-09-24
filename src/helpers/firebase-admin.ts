6ecbd88a7d7f432219fd9f9f29f9dd270c806ddc
Johnson Ubaezuonu <jaycodist@gmail.com>
2023-09-24 01:56:47 +0100

    added firebase hosting config

--- src/helpers/firebase-admin.ts
+++ src/helpers/firebase-admin.ts
@@ -1,10 +1,12 @@
 import firebaseAdmin from "firebase-admin";
+
 // eslint-disable-next-line import/no-unresolved
 import { applicationDefault } from "firebase-admin/app";
 
 firebaseAdmin.initializeApp({
   credential: applicationDefault(),
-  databaseURL: process.env.FIREBASE_DB_HOST
+  databaseURL: process.env.FIREBASE_DB_HOST,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 firebaseAdmin.firestore().settings({ ignoreUndefinedProperties: true });
 
