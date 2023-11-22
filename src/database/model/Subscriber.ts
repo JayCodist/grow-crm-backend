da71146ecad31897621d9d309f9edafbc55301c3
Johnson Ubaezuonu <jaycodist@gmail.com>
2023-11-22 16:11:53 +0100

    created subscriber model

---
+++ src/database/model/Subscriber.ts
@@ -0,0 +1,44 @@
+import { Schema, model, Document } from "mongoose";
+import Logger from "../../core/Logger";
+import { Business } from "./Order";
+
+const DOCUMENT_NAME = "Subscriber";
+const COLLECTION_NAME = "subscriber";
+
+export const subscriberProjection = ["email", "business"];
+
+export default interface Subscriber {
+  id: string;
+  email: string;
+  createdAt: string;
+  business: Business;
+}
+
+export interface SubscriberCreate extends Omit<Subscriber, "id"> {}
+
+interface SubscriberDocument extends Document, SubscriberCreate {}
+
+const schema = new Schema(
+  {
+    email: String,
+    business: { type: String, index: true },
+    createdAt: String
+  },
+  { skipVersioning: true }
+).index({
+  createdAt: 1
+});
+
+export const SubscriberModel = model<SubscriberDocument>(
+  DOCUMENT_NAME,
+  schema,
+  COLLECTION_NAME
+);
+
+SubscriberModel.on("index", err => {
+  if (err) {
+    Logger.error({ message: "Subscriber index error: ", err });
+  } else {
+    Logger.info("Subscriber indexing complete");
+  }
+});
