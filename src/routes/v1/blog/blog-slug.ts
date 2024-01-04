54e13bd6b2c0816653d35362f16fcc8d6fbe598d
Johnson Ubaezuonu <jaycodist@gmail.com>
2024-01-04 04:54:40 +0100

    added get-by-slug endpoint for blogs

---
+++ src/routes/v1/blog/blog-slug.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { ApiError } from "../../../core/ApiError";
+import { BadRequestResponse, SuccessResponse } from "../../../core/ApiResponse";
+import validator from "../../../helpers/validator";
+
+import validation from "./validation";
+import { Business } from "../../../database/model/Order";
+import BlogRepo from "../../../database/repository/BlogRepo";
+
+const blogSlug = express.Router();
+
+blogSlug.get(
+  "/slug/:slug",
+  validator(validation.business, "query"),
+  async (req, res) => {
+    try {
+      const { business } = req.query as {
+        business: Business;
+      };
+      const response = await BlogRepo.findBySlug(req.params.slug, business);
+
+      if (!response) {
+        new BadRequestResponse("Blog not found").send(res);
+        return;
+      }
+      new SuccessResponse("success", response).send(res);
+    } catch (error) {
+      ApiError.handle(error as Error, res);
+    }
+  }
+);
+
+export default blogSlug;
