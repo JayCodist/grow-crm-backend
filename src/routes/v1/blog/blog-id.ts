08244175af73b7f07fd7a69cd1bfb0cef8edfd13
Johnson Ubaezuonu <jaycodist@gmail.com>
2024-01-04 04:54:49 +0100

    added get-by-id endpoint for blogs

---
+++ src/routes/v1/blog/blog-id.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { ApiError } from "../../../core/ApiError";
+import { BadRequestResponse, SuccessResponse } from "../../../core/ApiResponse";
+import validator from "../../../helpers/validator";
+
+import validation from "./validation";
+import { Business } from "../../../database/model/Order";
+import BlogRepo from "../../../database/repository/BlogRepo";
+
+const blogId = express.Router();
+
+blogId.get(
+  "/:id",
+  validator(validation.business, "query"),
+  async (req, res) => {
+    try {
+      const { business } = req.query as {
+        business: Business;
+      };
+      const response = await BlogRepo.findById(req.params.id, business);
+
+      if (!response) {
+        new BadRequestResponse("Blog not found").send(res);
+        return;
+      }
+      new SuccessResponse("success", response).send(res);
+    } catch (error) {
+      ApiError.handle(error as Error, res);
+    }
+  }
+);
+
+export default blogId;
